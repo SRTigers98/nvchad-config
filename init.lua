c76c2fefcd85f4bd116a1569286ca43a538d88fd
Benjamin Eder <eder.ben01@gmail.com>
2023-09-14 08:23:45 +0200

    revert "create init vimscript"
    This reverts commit 29dc1c10d8b7ffe20b02cc8887e5d1a0e8711ddd.

--- init.lua
+++ init.lua
@@ -1,1 +1,7 @@
-vim.cmd "source init.vim"
+-- show spaces as dots
+vim.cmd "set list"
+vim.cmd "set lcs+=space:."
+
+-- set indentation
+vim.cmd "set expandtab"
+vim.cmd "set shiftwidth=2"
--- init.vim
+++
@@ -1,7 +0,0 @@
-" Indentation
-set expandtab
-set shiftwidth=2
-
-" Show spaces as dots
-set list
-set lcs+=space:.
