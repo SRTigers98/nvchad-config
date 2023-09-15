483a1adb3a4487099845589ec5307ca58c157adb
Benjamin Eder <eder.ben01@gmail.com>
2023-09-15 14:31:19 +0200

    install required packages script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Required packages
+packages=(
+  # Lua
+  "stylua"
+  "luacheck"
+  # Shell
+  "shfmt"
+  "shellcheck"
+)
+
+# Install packages
+for pck in "${packages[@]}"; do
+  if pacman -Qi "$pck" &>/dev/null; then
+    echo "$pck already installed"
+  else
+    echo "Installing $pck..."
+    pacman -S "$pck"
+  fi
+done
+
+# Go packages
+if go --help &>/dev/null; then
+  echo "Go isn't installed, skipping packages"
+else
+  go install golang.org/x/tools/gopls@latest
+fi
