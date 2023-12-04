200a42d40c592d2a2d900826718afe1cfefeea2f
Benjamin Eder <eder.ben01@gmail.com>
2023-12-04 10:39:52 +0100

    use <cmd> for mappings

--- mappings.lua
+++ mappings.lua
@@ -3,23 +3,23 @@ local M = {}
 -- Spell Checking
 M.spell = {
   n = {
-    ["<leader>sd"] = { ":set spell spelllang=de_de<CR>", "German Spell Check" },
-    ["<leader>se"] = { ":set spell spelllang=en_us<CR>", "English Spell Check" },
-    ["<leader>sx"] = { ":set nospell<CR>", "Disable Spell Check" },
+    ["<leader>sd"] = { "<cmd>set spell spelllang=de_de<CR>", "German Spell Check" },
+    ["<leader>se"] = { "<cmd>set spell spelllang=en_us<CR>", "English Spell Check" },
+    ["<leader>sx"] = { "<cmd>set nospell<CR>", "Disable Spell Check" },
   },
 }
 
 -- YAML Companion
 M.yaml = {
   n = {
-    ["<leader>ys"] = { ":Telescope yaml_schema<CR>", "Select YAML Schema" },
+    ["<leader>ys"] = { "<cmd>Telescope yaml_schema<CR>", "Select YAML Schema" },
   },
 }
 
 -- Git
 M.git = {
   n = {
-    ["<leader>gs"] = { ":AdvancedGitSearch<CR>", "Git Advanced Search" },
+    ["<leader>gs"] = { "<cmd>AdvancedGitSearch<CR>", "Git Advanced Search" },
   },
 }
 
