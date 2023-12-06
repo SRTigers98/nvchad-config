48e02d81453a90b90f9b5d4e1438000fea61e2c3
Benjamin Eder <eder.ben01@gmail.com>
2023-12-06 09:57:01 +0100

    add gopher mappings

--- mappings.lua
+++ mappings.lua
@@ -23,4 +23,19 @@ M.git = {
   },
 }
 
+-- Gopher
+M.gopher = {
+  plugin = true,
+  n = {
+    ["<leader>gsj"] = {
+      "<cmd> GoTagAdd json <CR>",
+      "Add json struct tags",
+    },
+    ["<leader>gsy"] = {
+      "<cmd> GoTagAdd yaml <CR>",
+      "Add yaml struct tags",
+    },
+  },
+}
+
 return M
