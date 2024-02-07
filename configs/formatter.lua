1577fa10935e9c0049b13c5215ff4efaf8bb94b3
Benjamin Eder <eder.ben01@gmail.com>
2024-02-07 10:10:20 +0100

    conditional js, ts, json formatting
    use prettier as formatter for js, ts and json if .prettierrc or
    .prettierrc.yaml is present

--- configs/formatter.lua
+++ configs/formatter.lua
@@ -1,47 +1,67 @@
 local util = require "formatter.util"
 
+local filetypes = {
+  go = {
+    require("formatter.filetypes.go").gofmt,
+    require("formatter.filetypes.go").goimports,
+  },
+  lua = {
+    require("formatter.filetypes.lua").stylua,
+  },
+  markdown = {
+    require("formatter.filetypes.markdown").prettier,
+  },
+  sh = {
+    require("formatter.filetypes.sh").shfmt,
+  },
+  tex = {
+    require("formatter.filetypes.latex").latexindent,
+  },
+  yaml = {
+    require("formatter.filetypes.yaml").prettier,
+  },
+  zig = {
+    require("formatter.filetypes.zig").zigfmt,
+  },
+  javascript = {
+    require("formatter.filetypes.javascript").biome,
+  },
+  typescript = {
+    require("formatter.filetypes.typescript").biome,
+  },
+  json = {
+    require("formatter.filetypes.json").biome,
+  },
+  elixir = {
+    require("formatter.filetypes.elixir").mixformat,
+  },
+  svelte = {
+    require("formatter.filetypes.svelte").prettier,
+  },
+  vue = {
+    require("formatter.filetypes.vue").prettier,
+  },
+  ["*"] = {
+    require("formatter.filetypes.any").remove_trailing_whitespace,
+  },
+}
+
+if vim.fs.find { ".prettierrc", ".prettierrc.yaml" } then
+  filetypes.javascript = {
+    require("formatter.filetypes.javascript").prettier,
+  }
+  filetypes.typescript = {
+    require("formatter.filetypes.typescript").prettier,
+  }
+  filetypes.json = {
+    require("formatter.filetypes.json").prettier,
+  }
+end
+
 require("formatter").setup {
   logging = true,
   log_level = vim.log.levels.WARN,
-  filetype = {
-    go = {
-      require("formatter.filetypes.go").gofmt,
-      require("formatter.filetypes.go").goimports,
-    },
-    lua = {
-      require("formatter.filetypes.lua").stylua,
-    },
-    markdown = {
-      require("formatter.filetypes.markdown").prettier,
-    },
-    sh = {
-      require("formatter.filetypes.sh").shfmt,
-    },
-    tex = {
-      require("formatter.filetypes.latex").latexindent,
-    },
-    yaml = {
-      require("formatter.filetypes.yaml").prettier,
-    },
-    zig = {
-      require("formatter.filetypes.zig").zigfmt,
-    },
-    javascript = {
-      require("formatter.filetypes.javascript").biome,
-    },
-    typescript = {
-      require("formatter.filetypes.typescript").biome,
-    },
-    json = {
-      require("formatter.filetypes.json").biome,
-    },
-    elixir = {
-      require("formatter.filetypes.elixir").mixformat,
-    },
-    ["*"] = {
-      require("formatter.filetypes.any").remove_trailing_whitespace,
-    },
-  },
+  filetype = filetypes,
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
