2d45766ce0d0c88c61a7f40fde7fa315cd9d5b8e
Benjamin Eder <eder.ben01@gmail.com>
2024-10-15 20:18:05 +0200

    configure nvim-tree
    fix nvim-tree

---
+++ lua/luna/plugins/nvim-tree.lua
@@ -0,0 +1,43 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    local nvimtree = require "nvim-tree"
+
+    -- recommended settings from nvim-tree documentation
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    nvimtree.setup {
+      view = {
+        width = 35,
+      },
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+      },
+      -- disable window_picker for
+      -- explorer to work well with
+      -- window splits
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = false,
+          },
+        },
+      },
+      filters = {
+        custom = { ".DS_Store" },
+        git_ignored = false,
+      },
+    }
+
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "<leader>e", "<cmd>NvimTreeFindFile<CR>", { desc = "Open file explorer on current file" })
+    keymap.set("n", "<leader>tc", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+    keymap.set("n", "<leader>tr", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+  end,
+}
